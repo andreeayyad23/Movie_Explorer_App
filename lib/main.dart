8e15fd4f8386c611f696a45bffb1a1557f91bc1f
Andreh,ayyad <andreh,ayad@gmail.com>
2024-12-27 01:06:54 +0300

    Create main screen with navigation and layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Movie Explorer App',
+      title: 'Movie Explorer App.',
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
