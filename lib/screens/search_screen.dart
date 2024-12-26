30c6b67fd97191696ec87ee6b346e7157d4d558a
Andreh,ayyad <andreh,ayad@gmail.com>
2024-12-27 01:00:23 +0300

    Create Search Screen with filtering and results

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -10,7 +10,7 @@ class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
+  State<SearchScreen> createState() => _SearchScreenState();;
 }
 
 class _SearchScreenState extends State<SearchScreen> {
