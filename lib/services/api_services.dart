f4a61b1718171fad99cd46daa0f9e89194df445a
Andreh,ayyad <andreh,ayad@gmail.com>
2024-12-28 17:35:00 +0300

    Add .env for security

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,21 +1,23 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:movie_explorer_app/common/utils.dart';
 import 'package:movie_explorer_app/models/describe_model.dart';
 import 'package:movie_explorer_app/models/movies_series.dart';
 import 'package:movie_explorer_app/models/search_model.dart';
 import 'package:movie_explorer_app/models/upcoming_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const String baseUrl = 'https://api.themoviedb.org/3/';
-var key = "?api_key=$apikey";
-late String endpoint;
 
 class ApiServices {
+  final String apiKey = dotenv.env['API_KEY']!; // Securely load the API key
+  late String endpoint;
+
+  // Fetch upcoming movies
   Future<UpcomingMovieModel> getUpcomingMovies() async {
     endpoint = "movie/upcoming";
-    final url = "$baseUrl$endpoint$key";
+    final url = "$baseUrl$endpoint?api_key=$apiKey";
 
     final response = await http.get(Uri.parse(url));
 
@@ -26,9 +28,10 @@ class ApiServices {
     throw Exception('Failed to load upcoming movies!');
   }
 
+  // Fetch now playing movies
   Future<UpcomingMovieModel> getNowPlayingMovies() async {
     endpoint = "movie/now_playing";
-    final url = "$baseUrl$endpoint$key";
+    final url = "$baseUrl$endpoint?api_key=$apiKey";
 
     final response = await http.get(Uri.parse(url));
 
@@ -39,9 +42,10 @@ class ApiServices {
     throw Exception('Failed to load now playing movies');
   }
 
+  // Fetch top-rated movies
   Future<MovieModel> getTopRatedMovies() async {
     endpoint = "movie/top_rated";
-    final url = "$baseUrl$endpoint$key";
+    final url = "$baseUrl$endpoint?api_key=$apiKey";
 
     final response = await http.get(Uri.parse(url));
 
@@ -52,13 +56,13 @@ class ApiServices {
     throw Exception('Failed to load top rated movies');
   }
 
+  // Search movies by text
   Future<SearchModel> getSearchMovies(String searchText) async {
     endpoint = "search/movie?query=$searchText";
-    final url = "$baseUrl$endpoint";
+    final url = "$baseUrl$endpoint&api_key=$apiKey";
     print("search url $url");
 
-    final response = await http.get(Uri.parse(url), headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjg4YTZhMWYwM2E5YzY5NWFjMTQzY2FlNDJiZDkyYiIsIm5iZiI6MTczNDk1NTUxMS4xMTksInN1YiI6IjY3Njk1MWY3NTdmNmE2N2M4NTVjZTkxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ujXY6aUQSojkU5t_PGPeTF1x2D1S8c-WJH3wy_UImMw'});
+    final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       log("Success");
@@ -67,17 +71,18 @@ class ApiServices {
     throw Exception('Failed to load search movies');
   }
 
-   Future<DescribeModel> getMovieDetails(int movieId) async {
+  // Fetch movie details by movie ID
+  Future<DescribeModel> getMovieDetails(int movieId) async {
     endpoint = "movie/$movieId";
-    final url = "$baseUrl$endpoint$key";
+    final url = "$baseUrl$endpoint?api_key=$apiKey";
     print("search url $url");
 
-    final response = await http.get(Uri.parse(url),);
+    final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       log("Success");
       return DescribeModel.fromJson(jsonDecode(response.body));
     }
-    throw Exception('Failed to load Describe movies');
+    throw Exception('Failed to load movie details');
   }
 }
